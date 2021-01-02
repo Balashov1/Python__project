331b248ee9bf53ea1e6c6065c175068f9df6bb98
Vadim <47371893+Balashov1@users.noreply.github.com>
2021-01-02 04:41:00 +0300

    dict
    And method

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
-# 13 - Исключения (Конструкция try - except)
+# Словари
 
-x = int(input())
-y = int(input())
+d = {'test': 1, 'test_2': "tes"}
+print(d)
 
-try:
-    res = x / y
-except ZeroDivisionError:
-    print("Вы ввели нуль")
-    res = 0
-print(res)
+a = dict(short='dict', longer='dictionary')
+a['short'] = 234
+print(a)
 
-try:
-    x = int(input())
-except ValueError:
-    print("Вы ввели не число!!!")
-    x = 0
-print(x)
+b = dict([(23, 34), (56, 87)])
+print(b)
 
-try:
-    x = int(input())
-except ValueError:
-    print("Вы ввели не число")
-    x = 0
-try:
-    y = int(input())
-except ValueError:
-    print("Вы ввели не число")
-    y = 0
-else:
-    print("OK")
-finally:
-    print("OK--100%")
+c = dict.fromkeys(['a', 'b'], 1)
+print(c)
 
-try:
-    res = x / y
-except ZeroDivisionError:
-    print("Вы ввели нуль")
-    res = 0
-print(res)
+e = {a: a ** 2 for a in range(7)}
+print(e)
+
+person = {'name': {'last_name': 'Ivan', 'first_name': 'Ivan',
+                   'middle_name': 'Ivanovich'}, 'address': ['city Andreyshkin',
+                                                            'st. Vasilkovskaya house 236',
+                                                            'ap.12'],
+          'phone': {'home_phone': '237-94-88', 'mobile_phone': '8-922-000-85-61',
+                    'mobile_phone2': 'No'
+                    }}
+print(person['phone']['mobile_phone2'])
+
+# person.clear()
+# print(person)
